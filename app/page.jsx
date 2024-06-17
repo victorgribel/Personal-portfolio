0bbb85283aab8dc9b0bff1a8be14ea0fdcdf1666
victor <victorlgribel@gmail.com>
2024-06-17 13:57:28 -0300

    herosection update

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ const Home = () => {
   return <section className="h-full">
     <div className="container mx-auto h-full">
       <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
-        <div className="text-center xl:text-left">
+        <div className="text-center xl:text-left order-2 xl:order-none">
           <span className="text-xl">Full-stack Developer</span>
           <h1 className="h1 mt-6">
             Olá, Eu sou <br/> <span>Victor Gribel</span>
@@ -14,7 +14,7 @@ const Home = () => {
           <p className="max-w[500px] mt-9 text-white/80 ">
           Sou um desenvolvedor Full Stack tenho 25 anos e com uma experiência em JavaScript (ES6) e PostgreSQL, juntamente com habilidades comprovadas em integração de sistemas via APIs e construção de APIs RESTful usando TypeORM, estou preparado para enfrentar qualquer desafio que a programação possa oferecer
           </p>
-         <div className=" flex flex-col xl:flex-row items-center gap-8">
+         <div className=" flex flex-col xl:flex-row items-center mt-5 gap-10">
             <Button variant="outline" size="lg" className="uppercase flex items-center gap-2">
               <span>Download CV</span>
               <FiDownload className="text-xl"/>
@@ -24,7 +24,9 @@ const Home = () => {
             </div>
           </div>
         </div>
-           <div><Photo/></div>
+           <div className="order-1 xl:order-none mb-8 xl:mb-8">
+            <Photo/>
+            </div>
       </div>
     </div>
    
--- components/Photo.jsx
+++ components/Photo.jsx
@@ -4,19 +4,38 @@ import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div className="w-full h-full relative">
+    <div className="w-full h-full relative ">
       <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { delay: 2, duration: 6.4, ease: "easeIn" } }}>
-        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[298px] mix-blend-lighten">
+        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"/>
           <Image
-            src="/assets/heroimg1.png"
+            src="/assets/heroimg3.png"
             priority
             quality={100}
-            width={400}
-            height={200}
+            fill
             className="object-contain"
             style={{opacity:0.6}}
           />
-        </div>
+        <motion.div>
+
+        </motion.div>
+
+        <motion.svg 
+            className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
+            fill="transparent"
+            viewBox="0 0 506 506"
+            xmlns="http://www.w3.org/2000/svg"
+        
+        >
+          <motion.circle cx="253" cy="253" r="250" stroke="#00ff99" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" 
+          initial={{strokeDasharray:"24 10 0 0"}} animate={{strokeDasharray:["15 120 25 25","16 25 92 72", "4 250 22 22"], rotate:[120,360]}}
+          transition={{
+            duration: 20,
+            repeat: Infinity,
+            repeatType: "reverse"
+          }}
+          
+          />
+        </motion.svg>
       </motion.div>
     </div>
   );
